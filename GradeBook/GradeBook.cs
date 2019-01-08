81b1fc43d29023f44cc1900acc6a423d8c451215
Pendragon <32424385+sprkbrn@users.noreply.github.com>
2019-01-08 06:02:51 -0500

    //comment main prog

---
+++ GradeBook/GradeBook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook
+{
+    class GradeBook
+    {
+        public GradeBook()
+        {
+            this.name = "Hello world!";
+        }
+
+        public string name;
+    }
+}
--- GradeBook/Program.cs
+++ GradeBook/Program.cs
@@ -6,21 +6,25 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            Student Joe = new Student("Joe", "Camel");
+            //Student Joe = new Student("Joe", "Camel");
 
-            Joe.AddClass("Science", 70f);
-            Joe.AddClass("English", 62.4f);
-            Joe.AddClass("Math", 54.8f);
+            //Joe.AddClass("Science", 70f);
+            //Joe.AddClass("English", 62.4f);
+            //Joe.AddClass("Math", 54.8f);
 
-            Console.WriteLine(Joe.GetFullName());
+            //Console.WriteLine(Joe.GetFullName());
 
-            foreach (Subject topic in Joe.GetAllClasses())
-            {
-                Console.WriteLine(topic);
-            }
+            //foreach (Subject topic in Joe.GetAllClasses())
+            //{
+            //    Console.WriteLine(topic);
+            //}
 
-            Console.WriteLine("Highest Grade: " + Joe.GetHighestGrade());
-            Console.WriteLine("Lowest Grade: " + Joe.GetLowestGrade());
+            //Console.WriteLine("Highest Grade: " + Joe.GetHighestGrade());
+            //Console.WriteLine("Lowest Grade: " + Joe.GetLowestGrade());
+
+            GradeBook my_grades = new GradeBook();
+
+            Console.WriteLine(my_grades.name);
         }
     }
 }
